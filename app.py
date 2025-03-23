364396b436af1d3a92806aae5b124ddbc44d0af2
emilydma08 <142754713+emilydma08@users.noreply.github.com>
2025-03-23 15:18:30 -0700

    Initial log functionality working

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template('index.html')
+
+@app.route("/log", methods=['POST'])
+def log():
+    if request.method == 'POST':
+        date = request.form['date']
+        title = request.form['title']
+        rating = request.form['rate']
+    return render_template('log.html', log_date=date, title=title, rate=rating)
 
 if __name__ == '__main__':
     app.run(debug=True)
