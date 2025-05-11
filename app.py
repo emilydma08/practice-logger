f6e1331c0d6e9990665030715fc113a90c253138
Emily Ma <emilyma@outlook.com>
2025-05-10 23:31:03 -0700

    Initial UI completed

--- app.py
+++ app.py
@@ -1,31 +1,52 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for
+from datetime import date
 
 app = Flask(__name__)
 
-class Log:
-    def __init__(self, date, title, rate):
-        self.date = date
-        self.title = title
-        self.rate = rate
+categories = [
+    {"icon": "🎸", "name": "Guitar", "description": "Practice your favorite songs and improve your skills!"},
+    {"icon": "📚", "name": "SAT Prep", "description": "Boost your score with focused study sessions."},
+    {"icon": "📚", "name": "SAT Prep", "description": "Boost your score with focused study sessions."},
+    {"icon": "📚", "name": "SAT Prep", "description": "Boost your score with focused study sessions."},
+    {"icon": "📚", "name": "SAT Prep", "description": "Boost your score with focused study sessions."},
+    {"icon": "📚", "name": "SAT Prep", "description": "Boost your score with focused study sessions."},
+    {"icon": "📚", "name": "SAT Prep", "description": "Boost your score with focused study sessions."},
+]
 
-    def __repr__(self):
-        return f"Log(date={self.date}, title={self.title}, rate={self.rate})"
-
+#Home Page
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', categories=categories)
 
-@app.route("/log", methods=['POST'])
-def log():
+#Create Category Form
+@app.route('/create-category', methods=['GET', 'POST'])
+def create_category():
     if request.method == 'POST':
-        date = request.form['date']
-        title = request.form['title']
-        rate = request.form['rate']
+        name = request.form['name']
+        icon = request.form['icon']
+        color = request.form['color']
+        # Save the new category...
+        return redirect(url_for('index'))
 
-        log_entry = Log(date, title, rate)
+    return render_template('create_category.html')
 
-    return render_template('log.html', log=log_entry)
+#Ind. Category Stats
+@app.route('/category_stats/<int:category_id>')
+def category_stats(category_id):
+    category = categories[category_id - 1]
+    return render_template('category_stats.html', category=category)
+
+#Logger Form
+@app.route('/log/<int:category_id>', methods=['GET', 'POST'])
+def logger_form(category_id):
+    category = categories[category_id - 1]    
+    current_date = date.today().isoformat()
+
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+
+    return render_template('logger_form.html', category=category, current_date=current_date)
 
 if __name__ == '__main__':
     app.run(debug=True)
